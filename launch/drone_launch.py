cee2c8b0036c01089f18fdc60fbdd75a5ff85a50
Eric Slaghuis <eslaghuis@gmail.com>
2021-09-22 12:53:42 +0200

    Removed Static Transform Publisher

--- launch/drone_launch.py
+++ launch/drone_launch.py
@@ -14,11 +14,7 @@ def generate_launch_description():
                 {"connection_url": "udp://:14540"}
              ]
            )
-    tf = Node(package = 'tf2_ros',
-                       executable = 'static_transform_publisher',
-                       arguments = ['0', '0', '0',  '0' , '0', '3.14159', 'odom', 'base_link'])
-
+   
     ld.add_action(node)
-    ld.add_action(tf)
 
     return ld
