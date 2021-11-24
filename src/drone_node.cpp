c0c1d6b0c2033592a1cff2af37327661c2fefa6a
Eric Slaghuis <eslaghuis@gmail.com>
2021-11-24 15:17:22 +0200

    Removed world-map transform

--- src/drone_node.cpp
+++ src/drone_node.cpp
@@ -1,3 +1,17 @@
+// Copyright 2021 Xeni Robotics
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -709,16 +723,19 @@ void DroneNode::init()
     
     auto message = sensor_msgs::msg::NavSatFix();       
     message.header.stamp = now;
-    message.header.frame_id ="map";      // Or should this be "world"?
+    message.header.frame_id ="world";
     message.status.status = sensor_msgs::msg::NavSatStatus::STATUS_FIX;
     message.status.service = sensor_msgs::msg::NavSatStatus::SERVICE_GPS; 
     message.latitude = position.latitude_deg;
     message.longitude = position.longitude_deg;
     message.altitude = position.absolute_altitude_m;   // Above MSL    
-    nav_sat_publisher_->publish(message);        
     // message.position_covariance[0] = 0.0;  // Leave blank
     message.position_covariance_type = sensor_msgs::msg::NavSatFix::COVARIANCE_TYPE_UNKNOWN;          
+    nav_sat_publisher_->publish(message);        
 
+    /*  Removed this capability now as it is incorrect
+        Should be something like a world->map transform
+        
     if (_have_global_origin) {
         
       // Transform current position
@@ -751,7 +768,7 @@ void DroneNode::init()
       t.transform.rotation.z = 0;
       t.transform.rotation.w = 1;
       tf_broadcaster_->sendTransform(t);
-    }
+    } */
     
   });
   
